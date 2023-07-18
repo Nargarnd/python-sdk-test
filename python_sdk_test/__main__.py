11377ff7048e6292430fe7fafffd76eef5fc1e52
Nargarnd <Nargarnd@users.noreply.github.com>
2023-07-18 09:38:50 +0000

    ✅ Ready to clone and code.

---
+++ python_sdk_test/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python_sdk_test."""
+
+from python_sdk_test.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from python_sdk_test.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "python_sdk_test"
